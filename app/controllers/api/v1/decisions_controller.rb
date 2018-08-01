8bf658044318a84837bb06f976ca95dc10b2375b
Jose Pedro Oliveira <joliveira@deec.uc.pt>
2018-08-01 23:59:18 +0100

    Added decisions controller

---
+++ app/controllers/api/v1/decisions_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::DecisionsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,45 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'decisions/new'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      get 'decisions/create'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      get 'decisions/update'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      get 'decisions/edit'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      get 'decisions/destroy'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      get 'decisions/show'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      get 'decisions/index'
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
