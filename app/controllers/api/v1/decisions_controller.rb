bd32be5b13fcefd7f83c905aaeced7270b92abec
Jose Pedro Oliveira <joliveira@deec.uc.pt>
2018-08-02 00:08:30 +0100

    Api controller get request teste

--- app/controllers/api/v1/decisions_controller.rb
+++ app/controllers/api/v1/decisions_controller.rb
@@ -1,4 +1,9 @@
 class Api::V1::DecisionsController < ApplicationController
+  
+	def index
+		render json: {status: 'SUCCESS', message: 'Loaded decisions', data:'{lixo:lixo}', status: :ok }
+  end
+
   def new
   end
 
@@ -17,6 +22,4 @@ class Api::V1::DecisionsController < ApplicationController
   def show
   end
 
-  def index
-  end
 end
