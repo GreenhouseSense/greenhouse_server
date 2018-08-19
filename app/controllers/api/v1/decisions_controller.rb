277c260568a9e08f1bbf7cbd5133226d79385f2a
Jose Pedro Oliveira <joliveira@deec.uc.pt>
2018-08-19 17:42:26 +0100

    Developed and tested decisions_controller methods

--- app/controllers/api/v1/decisions_controller.rb
+++ app/controllers/api/v1/decisions_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::DecisionsController < ApplicationController
   def add
-    # Método POST para adicionar uma decisão à bd
-    new_decision = Decision.new(decision_params)
+    # POST Method to add a new decision registry on bd
+    new_decision = Decision.new(add_decision_params)
     if new_decision.save
       render json: {status: 'SUCCESS', message: 'Added new decision', data: new_decision}, status: :ok 
     else
@@ -10,26 +10,63 @@ class Api::V1::DecisionsController < ApplicationController
   end
 
   def get_last
+    # GET Method to retreive the last decision added
     last_decision = Decision.order("created_at").last
     render json: {status: 'SUCCESS', message: 'Loaded decisions', data: last_decision},status: :ok
   end
 
   def get_weeks
-    # Método GET para obter as decições das últimas X semanas
+    # GET Method to retreive the decision added in last X weeks
+    weeks = params[:weeks].to_i
+    if weeks > 0
+      week_days = weeks * 7
+      weeks_decisions = Decision.where('created_at >= ?', Time.zone.now - week_days.days) 
+      if weeks_decisions == 0
+        msg_str = "No records for last #{weeks} weeks"      
+      else
+        msg_str = "Loaded decisions from last #{weeks} weeks"
+      end
+      render json: {status: 'SUCCESS', message: msg_str, data: weeks_decisions},status: :ok
+    else
+      render json: {status: 'SUCCESS', message: "Weeks argument must be a integer higher than 0", data: 0},status: :ok
+    end
   end
 
   def get_months
-    # Método GET para obter as decições dos últimos Y meses
+    # GET Method to retreive the decision added in last X months
+    months = params[:months].to_i
+    if months > 0
+      months_decisions = Decision.where('created_at >= ?', Time.zone.now - months.months) 
+      if months_decisions == 0
+        msg_str = "No records for last #{months} months"      
+      else
+        msg_str = "Loaded decisions from last #{months} months"
+      end
+      render json: {status: 'SUCCESS', message: msg_str, data: months_decisions},status: :ok
+    else
+      render json: {status: 'SUCCESS', message: "Month argument must be a integer higher than 0", data: 0},status: :ok
+    end
   end
 
   def get_years
-    # Método GET para obter as decições dos últimos K anos
+    # GET Method to retreive the decision added in last X years
+    years = params[:years].to_i
+    if years > 0
+      years_decisions = Decision.where('created_at >= ?', Time.zone.now - 1.years) 
+      if years_decisions == 0
+        msg_str = "No records for last #{years} years"      
+      else
+        msg_str = "Loaded decisions from last #{years} years"
+      end
+      render json: {status: 'SUCCESS', message: msg_str, data: years_decisions},status: :ok
+    else
+      render json: {status: 'SUCCESS', message: "Years argument must be a integer higher than 0", data: 0},status: :ok
+    end
   end
 
   private
    # Never trust parameters from the scary internet, only allow the white list through.
-    def decision_params
-      
+    def add_decision_params      
       params.permit(:origin, :description)
     end
 
--- app/controllers/api/v1/readings_controller.rb
+++ app/controllers/api/v1/readings_controller.rb
@@ -1,8 +1,17 @@
 class Api::V1::ReadingsController < ApplicationController
   def add
+    new_reading = Reading.new(add_reading_params)
+    if new_reading.save
+      render json: {status: 'SUCCESS', message: 'Added new reading', data: new_reading}, status: :ok 
+    else
+      render json: {status: 'ERROR', message: 'Reading not added', data: new_reading.errors}, status: :unprocessable_entity
+    end
   end
 
   def get_last
+    # GET Method to retreive the last decision added
+    last_reading = Reading.order("created_at").last
+    render json: {status: 'SUCCESS', message: 'Loaded readings', data: last_reading},status: :ok
   end
 
   def get_weeks
@@ -13,4 +22,16 @@ class Api::V1::ReadingsController < ApplicationController
 
   def get_years
   end
+
+
+  private
+   # Never trust parameters from the scary internet, only allow the white list through.
+    def add_reading_params      
+      params.permit(:air_humidity, :air_temperature, :soil_moisture, 
+                    :red_light_intensity, :blue_light_intensity, 
+                    :green_light_intensity, :outside_light_intensity, 
+                    :fan_status, :roof_door_position, :front_door_status, 
+                    :wattering_status)
+    end
+
 end
