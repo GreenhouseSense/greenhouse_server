7341c4e22579aef937d57a365a414b51d315ebae
Jose Pedro Oliveira <joliveira@deec.uc.pt>
2018-08-19 18:32:33 +0100

    Developed reading controller methods

--- app/controllers/api/v1/readings_controller.rb
+++ app/controllers/api/v1/readings_controller.rb
@@ -9,18 +9,101 @@ class Api::V1::ReadingsController < ApplicationController
   end
 
   def get_last
-    # GET Method to retreive the last decision added
-    last_reading = Reading.order("created_at").last
-    render json: {status: 'SUCCESS', message: 'Loaded readings', data: last_reading},status: :ok
+    # GET Method to retreive the last readings added
+    if Reading.count.to_i = 0
+      render json: {status: 'ERROR', message: 'No readings found', data: 0},status: :ok
+    else
+      last_reading = Reading.order("created_at").last
+      render json: {status: 'SUCCESS', message: 'Loaded readings', data: last_reading},status: :ok
+    end
   end
 
   def get_weeks
+    # GET Method to retreive the readings added in last X weeks
+    begin
+      weeks = params[:weeks].to_i
+    rescue 
+        render json: {status: 'ERROR', message: "Weeks argument must be a integer", data: 0},status: :ok
+        return true
+    end
+
+    if weeks <= 0
+      render json: {status: 'ERROR', message: "Weeks argument must be a integer higher than 0", data: 0},status: :ok
+      return true
+    end
+
+    if Reading.count. == 0
+      render json: {status: 'ERROR', message: 'No readings found', data: 0},status: :ok
+      return true
+    end
+      
+    week_days = weeks * 7
+    weeks_readings = Reading.where('created_at >= ?', Time.zone.now - week_days.days) 
+    if weeks_readings == 0
+      msg_str = "No records for last #{weeks} weeks"      
+    else
+      msg_str = "Loaded readings from last #{weeks} weeks"
+    end
+    render json: {status: 'SUCCESS', message: msg_str, data: weeks_readings},status: :ok
+    return true
   end
 
   def get_months
+    # GET Method to retreive the decision added in last X months
+    begin
+      months = params[:months].to_i
+    rescue 
+        render json: {status: 'ERROR', message: "Months argument must be a integer", data: 0},status: :ok
+        return true
+    end
+
+    if months <= 0
+      render json: {status: 'ERROR', message: "Months argument must be a integer higher than 0", data: 0},status: :ok
+      return true
+    end
+
+    if Reading.count == 0
+      render json: {status: 'ERROR', message: 'No readings found', data: 0},status: :ok
+      return true
+    end
+
+    months_readings = Reading.where('created_at >= ?', Time.zone.now - months.months) 
+    if months_readings == 0
+        msg_str = "No readings for last #{months} months"      
+      else
+        msg_str = "Loaded readings from last #{months} months"
+      end
+      render json: {status: 'SUCCESS', message: msg_str, data: months_readings},status: :ok
+    return true
   end
 
   def get_years
+    # GET Method to retreive the decision added in last X years
+    begin
+      years = params[:years].to_i
+    rescue 
+      render json: {status: 'ERROR', message: "Years argument must be a integer", data: 0},status: :ok
+      return true
+    end
+
+    if years <= 0
+      render json: {status: 'ERROR', message: "Years argument must be a integer higher than 0", data: 0},status: :ok
+      return true
+    end
+
+    if Reading.count == 0
+      render json: {status: 'ERROR', message: 'No readings found', data: 0},status: :ok
+      return true
+    end
+
+    years_readings = Reading.where('created_at >= ?', Time.zone.now - years.years) 
+    if years_readings == 0
+      msg_str = "No readings for last #{years} years"      
+    else
+      msg_str = "Loaded readings from last #{years} years"
+    end
+    render json: {status: 'SUCCESS', message: msg_str, data: years_readings},status: :ok
+    return true
   end
 
 
