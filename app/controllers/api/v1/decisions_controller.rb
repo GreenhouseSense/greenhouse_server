476e4c742317ada41bc05979690d75562318581b
Jose Pedro Oliveira <joliveira@deec.uc.pt>
2018-08-19 18:34:07 +0100

    Clean up on decisions controller

--- app/controllers/api/v1/decisions_controller.rb
+++ app/controllers/api/v1/decisions_controller.rb
@@ -11,57 +11,100 @@ class Api::V1::DecisionsController < ApplicationController
 
   def get_last
     # GET Method to retreive the last decision added
-    last_decision = Decision.order("created_at").last
-    render json: {status: 'SUCCESS', message: 'Loaded decisions', data: last_decision},status: :ok
+    if Decision.count.to_i = 0
+      render json: {status: 'ERROR', message: 'No decisions found', data: 0},status: :ok
+    else
+      last_decision = Decision.order("created_at").last
+      render json: {status: 'SUCCESS', message: 'Loaded decision', data: last_decision,status: :ok
+    end
   end
 
   def get_weeks
     # GET Method to retreive the decision added in last X weeks
-    weeks = params[:weeks].to_i
-    if weeks > 0
-      week_days = weeks * 7
-      weeks_decisions = Decision.where('created_at >= ?', Time.zone.now - week_days.days) 
-      if weeks_decisions == 0
-        msg_str = "No records for last #{weeks} weeks"      
-      else
-        msg_str = "Loaded decisions from last #{weeks} weeks"
-      end
-      render json: {status: 'SUCCESS', message: msg_str, data: weeks_decisions},status: :ok
-    else
-      render json: {status: 'SUCCESS', message: "Weeks argument must be a integer higher than 0", data: 0},status: :ok
+    begin
+      weeks = params[:weeks].to_i
+    rescue 
+        render json: {status: 'ERROR', message: "Weeks argument must be a integer", data: 0},status: :ok
+        return true
     end
+
+    if weeks <= 0
+      render json: {status: 'ERROR', message: "Weeks argument must be a integer higher than 0", data: 0},status: :ok
+      return true
+    end
+
+    if Decision.count. == 0
+      render json: {status: 'ERROR', message: 'No readings found', data: 0},status: :ok
+      return true
+    end
+      
+    week_days = weeks * 7
+    weeks_decisions = Decision.where('created_at >= ?', Time.zone.now - week_days.days) 
+    if weeks_decisions == 0
+      msg_str = "No decisions for last #{weeks} weeks"      
+    else
+      msg_str = "Loaded decisions from last #{weeks} weeks"
+    end
+    render json: {status: 'SUCCESS', message: msg_str, data: weeks_decisions},status: :ok
+    return true
   end
 
   def get_months
     # GET Method to retreive the decision added in last X months
-    months = params[:months].to_i
-    if months > 0
-      months_decisions = Decision.where('created_at >= ?', Time.zone.now - months.months) 
-      if months_decisions == 0
-        msg_str = "No records for last #{months} months"      
+    begin
+      months = params[:months].to_i
+    rescue 
+        render json: {status: 'ERROR', message: "Months argument must be a integer", data: 0},status: :ok
+        return true
+    end
+
+    if months <= 0
+      render json: {status: 'ERROR', message: "Months argument must be a integer higher than 0", data: 0},status: :ok
+      return true
+    end
+
+    if Decision.count == 0
+      render json: {status: 'ERROR', message: 'No readings found', data: 0},status: :ok
+      return true
+    end
+
+    months_decisions = Decision.where('created_at >= ?', Time.zone.now - months.months) 
+    if months_decisions == 0
+        msg_str = "No decisions for last #{months} months"      
       else
         msg_str = "Loaded decisions from last #{months} months"
       end
       render json: {status: 'SUCCESS', message: msg_str, data: months_decisions},status: :ok
-    else
-      render json: {status: 'SUCCESS', message: "Month argument must be a integer higher than 0", data: 0},status: :ok
-    end
+    return true
   end
 
   def get_years
     # GET Method to retreive the decision added in last X years
-    years = params[:years].to_i
-    if years > 0
-      years_decisions = Decision.where('created_at >= ?', Time.zone.now - 1.years) 
-      if years_decisions == 0
-        msg_str = "No records for last #{years} years"      
-      else
-        msg_str = "Loaded decisions from last #{years} years"
-      end
-      render json: {status: 'SUCCESS', message: msg_str, data: years_decisions},status: :ok
-    else
-      render json: {status: 'SUCCESS', message: "Years argument must be a integer higher than 0", data: 0},status: :ok
+    begin
+      years = params[:years].to_i
+    rescue 
+      render json: {status: 'ERROR', message: "Years argument must be a integer", data: 0},status: :ok
+      return true
     end
+
+    if years <= 0
+      render json: {status: 'ERROR', message: "Years argument must be a integer higher than 0", data: 0},status: :ok
+      return true
+    end
+
+    if Decision.count == 0
+      render json: {status: 'ERROR', message: 'No decisions found', data: 0},status: :ok
+      return true
+    end
+
+    years_decisions = Decision.where('created_at >= ?', Time.zone.now - years.years) 
+    if years_decisions == 0
+      msg_str = "No decisions for last #{years} years"      
+    else
+      msg_str = "Loaded decisions from last #{years} years"
+    end
+    render json: {status: 'SUCCESS', message: msg_str, data: years_decisions},status: :ok
+    return true
   end
 
   private
