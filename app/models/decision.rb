f79fce5202e27a378581f0dddd725d1011a6a0ea
Jose Pedro Oliveira <joliveira@deec.uc.pt>
2018-08-23 15:30:40 +0100

    Removed format verification for decisions model

--- app/models/decision.rb
+++ app/models/decision.rb
@@ -1,6 +1,6 @@
 class Decision < ApplicationRecord
 	#
 	#/\A\[[+-]?\d,+\]+\z/
-	validates :description, presence: true, format: { with: /\A+\[[0-9,\s]+\]+\z/, message: "Integer only. No sign allowed." }
+	validates :description, presence: true #, format: { with: /\A+\[[0-9,\s]+\]+\z/, message: "Integer only. No sign allowed." }
 	validates :origin, presence: true
 end
